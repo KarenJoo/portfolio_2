cfe4d62f332c7f3700154010cc2c8f5c9f8a0d0a
Karen Jo <karenfiksdahl@gmail.com>
2024-05-31 14:51:21 +0200

    adds inner scroll fro modal

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,32 +1,32 @@
-import { burgerMenu } from "./nav/navbar.mjs";
-import { initModals } from './modals/index.js';
-
+import { initModals } from './modals/index.js'
+import { burgerMenu } from './nav/navbar.mjs'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const menuIcon = document.querySelector('.menu-icon');
-  menuIcon.addEventListener('click', burgerMenu);
-  initModals();  
-  
-  const video = document.getElementById('mediaVideo');
-  const placeholder = document.getElementById('video-placeholder');
+  const menuIcon = document.querySelector('.menu-icon')
+  menuIcon.addEventListener('click', burgerMenu)
+  initModals()
 
-  
+  const video = document.getElementById('mediaVideo')
+  const placeholder = document.getElementById('video-placeholder')
+
   if (video && placeholder) {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          video.playbackRate = 0.5;
-          video.style.display = 'block'; // Show video
-          placeholder.style.display = 'none'; // Hide placeholder
-          video.play();
-          observer.unobserve(video);
-        }
-      });
-    }, {
-      threshold: 0.1
-    });
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            video.playbackRate = 0.5
+            video.style.display = 'block' // Show video
+            placeholder.style.display = 'none' // Hide placeholder
+            video.play()
+            observer.unobserve(video)
+          }
+        })
+      },
+      {
+        threshold: 0.1
+      }
+    )
 
-    observer.observe(video);
+    observer.observe(video)
   }
-});
-
+})
