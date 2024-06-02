582c555d6aa5161dc2cd53d833222681cf7a48e4
Karen Jo <karenfiksdahl@gmail.com>
2024-06-02 23:07:01 +0200

    adjusts modal window styling

--- src/js/modals/index.js
+++ src/js/modals/index.js
@@ -1,29 +1,29 @@
 export function initModals() {
-    const viewMoreBtns = document.querySelectorAll('.view-more-btn');
-    const modals = document.querySelectorAll('.modal');
-    const closeBtns = document.querySelectorAll('.close');
-  
-    viewMoreBtns.forEach((btn, index) => {
-        btn.addEventListener('click', () => {
-          modals[index].style.display = 'flex';
-          document.body.style.overflow = 'hidden'; 
-        });
-      });
-    
-      closeBtns.forEach((btn) => {
-        btn.addEventListener('click', () => {
-          const modal = btn.closest('.modal');
-          modal.style.display = 'none';
-          document.body.style.overflow = ''; 
-        });
-      });
-    
-      window.addEventListener('click', (event) => {
-        modals.forEach((modal) => {
-          if (event.target == modal) {
-            modal.style.display = 'none';
-            document.body.style.overflow = ''; 
-          }
-        });
-      });
-    }
+  const viewMoreBtns = document.querySelectorAll('.view-more-btn')
+  const modals = document.querySelectorAll('.modal')
+  const closeBtns = document.querySelectorAll('.close')
+
+  viewMoreBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      modals[index].style.display = 'flex'
+      document.body.classList.add('open') // Prevent background scroll
+    })
+  })
+
+  closeBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const modal = btn.closest('.modal')
+      modal.style.display = 'none'
+      document.body.classList.remove('open') // Allow background scroll
+    })
+  })
+
+  window.addEventListener('click', (event) => {
+    modals.forEach((modal) => {
+      if (event.target === modal) {
+        modal.style.display = 'none'
+        document.body.classList.remove('open') // Allow background scroll
+      }
+    })
+  })
+}
