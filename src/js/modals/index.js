2fc12605c0c2eddeb166e77f25ccda2e6d6a2def
Karen Jo <karenfiksdahl@gmail.com>
2024-06-03 20:54:43 +0200

    creates modal 2, adds images and styling

--- src/js/modals/index.js
+++ src/js/modals/index.js
@@ -1,33 +1,32 @@
 export function initModals() {
-  const viewMoreBtns = document.querySelectorAll('.view-more-btn')
-  const modals = document.querySelectorAll('.modal')
-  const closeBtns = document.querySelectorAll('.close')
-  const overlay = document.querySelector('.overlay')
+  const viewMoreBtns = document.querySelectorAll('.view-more-btn');
+  const modals = document.querySelectorAll('.modal');
+  const closeBtns = document.querySelectorAll('.close');
+  const overlays = document.querySelectorAll('.overlay');
 
   viewMoreBtns.forEach((btn, index) => {
     btn.addEventListener('click', () => {
-      modals[index].style.display = 'flex'
-      overlay.style.display = 'block'
-      document.body.classList.add('open')
-    })
-  })
+      modals[index].style.display = 'flex';
+      overlays[index].style.display = 'block';
+      document.body.classList.add('open');
+    });
+  });
 
   closeBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
-      const modal = btn.closest('.modal')
-      modal.style.display = 'none'
-      overlay.style.display = 'none'
-      document.body.classList.remove('open')
-    })
-  })
+      const modal = btn.closest('.modal');
+      const overlay = modal.previousElementSibling;
+      modal.style.display = 'none';
+      overlay.style.display = 'none';
+      document.body.classList.remove('open');
+    });
+  });
 
-  window.addEventListener('click', (event) => {
-    modals.forEach((modal) => {
-      if (event.target === modal || event.target === overlay) {
-        modal.style.display = 'none'
-        overlay.style.display = 'none'
-        document.body.classList.remove('open')
-      }
-    })
-  })
+  overlays.forEach((overlay, index) => {
+    overlay.addEventListener('click', () => {
+      modals[index].style.display = 'none';
+      overlay.style.display = 'none';
+      document.body.classList.remove('open');
+    });
+  });
 }
