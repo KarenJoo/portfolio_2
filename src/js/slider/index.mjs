26fa35a7a1231830312b4f164afb92ad61aada95
Karen Jo <karenfiksdahl@gmail.com>
2024-06-08 19:23:03 +0200

    slider works, centers card

--- src/js/slider/index.mjs
+++ src/js/slider/index.mjs
@@ -1,20 +1,19 @@
 let currentSlide = 0;
 
-export default function moveSlide(direction) {
+function moveSlide(direction) {
   const slides = document.querySelectorAll('.card-container');
   const totalSlides = slides.length;
 
-  // Update the current slide index
   currentSlide += direction;
 
-  // Wrap around the slide index if out of bounds
   if (currentSlide < 0) {
     currentSlide = totalSlides - 1;
   } else if (currentSlide >= totalSlides) {
     currentSlide = 0;
   }
 
-  // Calculate the offset and apply transform
   const offset = -currentSlide * 100;
   document.querySelector('.projects-grid').style.transform = `translateX(${offset}%)`;
 }
+
+export default moveSlide;
