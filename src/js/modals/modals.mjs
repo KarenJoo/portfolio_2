cb0490f549e5b1b602c522da047d1d575651d5f9
Karen Jo <karenfiksdahl@gmail.com>
2024-06-09 13:53:50 +0200

    moduls display correct on each slide

--- src/js/modals/modals.mjs
+++ src/js/modals/modals.mjs
@@ -1,12 +1,14 @@
-
 export function initModals() {
     const openModalButtons = document.querySelectorAll('[data-modal-target]');
     const closeModalButtons = document.querySelectorAll('[data-close-button]');
     
     openModalButtons.forEach(button => {
       button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget);
-        openModal(modal);
+        const modalId = button.dataset.modalTarget;
+        const modal = document.querySelector(modalId);
+        const cardContainerId = button.dataset.cardContainerId; 
+        const cardContainer = document.getElementById(cardContainerId);
+        openModal(modal, cardContainer);
       });
     });
   
@@ -17,10 +19,20 @@ export function initModals() {
       });
     });
   
-    function openModal(modal) {
-      if (modal == null) return;
-      modal.style.display = 'block';
+    function openModal(modal, cardContainer) {
+        if (modal == null || cardContainer == null) return;
+        modal.style.display = 'block';
+        modal.style.position = 'fixed';
+        modal.style.left = '50%';
+        modal.style.top = '50%';
+        modal.style.marginTop = '20px';
+        modal.style.transform = 'translate(-50%, -50%)';
+        document.body.appendChild(modal);
     }
+    
+    
+    
+    
   
     function closeModal(modal) {
       if (modal == null) return;
@@ -33,3 +45,4 @@ export function initModals() {
       }
     });
   }
+  
